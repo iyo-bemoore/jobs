41048ffbc562694264ba89092c8a86950f882e65
2imad <imadyoussoufi@gmail.com>
2020-07-21 11:51:39 +0200

    add README

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -2,28 +2,25 @@ import axios from "axios";
 import { FETCH_JOBS } from "./types";
 import reverseGeocode from "latlng-to-zip";
 import qs from "qs";
-
-const INDEED_API = "8769962633668405";
-const INDEED_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
-const GOOGL_MAPS_API = "AIzaSyCjEjD_MJ70jXHuhR7T62ZV6ZDRIokMihE";
+import { keys } from "./keys";
 
 const JOB_QUERY_PARAMS = {
-  publisher: INDEED_API,
+  publisher: keys.INDEED_API,
   format: "json",
   v: "2",
   latlong: 1,
   radius: 10,
-  q: "javascript"
+  q: "javascript",
 };
 
-const constructUrl = zip => {
+const constructUrl = (zip) => {
   const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
-  return `${INDEED_ROOT_URL}${query}`;
+  return `${keys.INDEED_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = region => async dispatch => {
+export const fetchJobs = (region) => async (dispatch) => {
   try {
-    let zip = await reverseGeocode(region, GOOGL_MAPS_API);
+    let zip = await reverseGeocode(region, keys.GOOGL_MAPS_API);
     const url = constructUrl(zip);
     let { data } = await axios.get(url);
     dispatch({ type: FETCH_JOBS, payload: data });
